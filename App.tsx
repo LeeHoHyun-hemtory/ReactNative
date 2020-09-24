fe787adaaa796f2e5f6244a18199ea6651aa6a76
lhh <jyslshsgdy@gmail.com>
2020-09-24 16:47:51 +0900

    To Do List first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
+import Todo from './Todo';
+
+type item = {
+  id: number;
+  todo: string
+};
+
+type itemList = item[]
+
 
 export default function App() {
+  const [list, setList] = useState<itemList>([]);
+  const [todo, setTodo] = useState('');
+  const [id, setId] = useState(1);
+
+  const onChangeInput = (text: string) => {
+    setTodo(text);
+  };
+
+  const onPressAdd = () => {
+    if(todo){
+      const newArr = [ ...list, { id, todo } ]
+      setList(newArr);
+      setTodo('');
+      setId(id + 1);
+    }
+  }
+
+  const onPressRemove = (key: number) => {
+    setList(list.filter(el => el.id !== key))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={[styles.flexRow, {height: 100}]}>
+        <View style={[styles.flexCenter, {flex: 1, backgroundColor: 'pink'}]}>
+          <Text style={styles.todolistText}>To Do List</Text>
+        </View>
+      </View>
+      <View style={[styles.flexRow, {marginTop: 50}]}>
+        <TextInput onChangeText={onChangeInput} value={todo} style={styles.inputStyle}/>
+        <TouchableOpacity onPress={onPressAdd} 
+        style={[styles.flexCenter, styles.btnAdd, {marginLeft: 20}]}>
+          <Text style={styles.btnAddText}>추가</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{marginTop: 30}}>
+        {list.map((el: item) => <Todo key={el.id} todo={el.todo} onPressRemove={onPressRemove} id={el.id} />)}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +60,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  flexCenter: {
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  flexRow: {
+    flexDirection: 'row'
+  },
+  todolistText: {
+    fontSize: 30, 
+    fontWeight: "bold", 
+    color:'white', 
+    textAlign:'center'
+  },
+  inputStyle: {
+    width: 300, 
+    borderBottomColor: 'pink', 
+    borderBottomWidth: 1
+  },
+  btnAdd: {
+    width: 70, 
+    height: 40, 
+    backgroundColor: 'pink'
+  },
+  btnAddText: {
+    color: 'white', 
+    fontWeight: 'bold',
+    fontSize: 30
+  }
 });
---
+++ Todo.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App(props: any) {
+  const { id, todo, onPressRemove } = props;
+  const [done, setDone] = useState(false);
+
+  const onPressToggle = () => {
+    setDone(!done);
+  }
+
+  return (
+    <View key={id} style={styles.todo}>
+      <TouchableOpacity onPress={onPressToggle}>
+        <Text style={styles.list}>{todo}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => onPressRemove(id)} style={[styles.flexCenter, styles.btnRemove]}>
+        <Text style={styles.RemoveFont}>삭제</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  flexCenter: {
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  todo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    width: 350, 
+    height: 50, 
+    backgroundColor: 'pink', 
+    marginTop: 20
+  },
+  done: {
+    textDecorationLine: 'line-through',
+    textDecorationColor: 'pink',
+    color: '#888'
+  },
+  list: {
+    fontSize: 40, 
+    fontWeight: 'bold', 
+    color: 'white', 
+    marginLeft: 10
+  },
+  btnRemove: {
+    width: 60, 
+    height: 40, 
+    borderWidth: 1, 
+    borderColor: 'white', 
+    padding: 3, 
+    marginRight: 5
+  },
+  RemoveFont: {
+    fontSize: 25, 
+    color: 'white'
+  }
+});
